85fdcf5ff9f1e96fe5acb807994e547c879b7202
Amanuel <amanuel.ayalew@aait.edu.et>
2024-01-05 18:17:38 +0300

    solution for the Excercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+   
+    let string = word.split('')
+    x = string.reverse();
+    y = x.join('');
+    
+    return y;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const resultArray = [...array];
+    
+    for (let i = 0; i < args.length; i++) {
+        const removedItem = resultArray.indexOf(args[i]);
 
+        if (removedItem !== -1) {
+            resultArray.splice(removedItem, 1);
+        }
+    }
+
+    return resultArray;
 };
 
 // Do not edit below this line
