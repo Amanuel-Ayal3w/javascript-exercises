56ec0ab5cf5b93be7cfd1872c0200db323260641
Amanuel <amanuel.ayalew@aait.edu.et>
2024-01-05 19:23:30 +0300

    solutions Done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+
+  const convertToCelsius = (5/9) * (fahrenheit - 32 )
+
+  const rounded = Math.round(convertToCelsius*10)/10
+
+  return rounded
+
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+  const convertToFahrenheit = ((9/5)*celsius) + 32 ;
+
+  const rounded = Math.round(convertToFahrenheit*10)/10;
+
+  return rounded;
+
+
 };
 
 // Do not edit below this line
